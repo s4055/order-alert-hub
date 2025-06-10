2013bfe0c7e04790630bdffb66260418ae28326a
s4055 <sijin901@gmail.com>
2025-06-10 14:16:40 +0900

    feat: project init
    feat: project init

---
+++ src/main/java/com/example/orderalerthub/OrderAlertHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderalerthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAlertHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderAlertHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orderalerthub/OrderAlertHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderalerthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderAlertHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
